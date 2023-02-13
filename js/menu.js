a183c660f90c154affd76eebe8e63d9275be228e
Gabriel Goulart Pereira <gabrielgoulartpereira23@gmail.com>
2023-02-13 17:30:07 -0300

    Ajustes no nome das classes

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,22 @@ const DOM = {
   list: document.querySelector(".interactive-content"),
   sectionButtons: document.querySelectorAll(".interactive-selector > button"),
   render(items) {
-    this.list.innerHTML = '';
+    this.list.innerHTML = "";
     items.forEach((item) => {
-      this.list.appendChild(DomElementBuilder.item(item.name, item.description));
+      this.list.appendChild(
+        DomElementBuilder.item(item.name, item.description)
+      );
     });
   },
+  toggleSection: function (index) {
+    this.sectionButtons.forEach((button) => {
+      button.classList.remove("j-enable");
+    });
+    this.sectionButtons[index].classList.add("j-enable");
+  },
   toggleParagraph: function (element) {
-    element.classList.toggle('j-active');
-  }
+    element.classList.toggle("j-enable");
+  },
 };
 
 const DomElementBuilder = {
@@ -21,11 +29,17 @@ const DomElementBuilder = {
     img.width = width;
     img.height = height;
     return img;
-  }, 
+  },
   item: function (title, description) {
-    // <img> 
-    let imgDecoration = this.img("img/svg/decorator-brown.svg", "interactive-decorator", 12, 12, "Decorador");
-    // <h2> 
+    // <img>
+    let imgDecoration = this.img(
+      "img/svg/decorator-brown.svg",
+      "interactive-decorator",
+      12,
+      12,
+      "Decorador"
+    );
+    // <h2>
     let h2 = document.createElement("h2");
     h2.classList.add("t2-m", "b5");
     h2.innerText = title;
@@ -34,17 +48,23 @@ const DomElementBuilder = {
     p.classList.add("t2-s", "b6");
     p.innerText = description;
     // <img>
-    let imgArrow = this.img("img/svg/arrow.svg", "interactive-arrow", 16, 20, "Flecha");
+    let imgArrow = this.img(
+      "img/svg/arrow.svg",
+      "interactive-arrow",
+      20,
+      16,
+      "Flecha"
+    );
     // <button>
     let button = document.createElement("button");
-    button.addEventListener('click', interactiveMenu.descriptionButtonHandler);
+    button.addEventListener("click", interactiveMenu.descriptionButtonHandler);
     button.append(imgDecoration, h2, imgArrow, p);
     // <li>
     let li = document.createElement("li");
     li.appendChild(button);
     return li;
-  }
-}
+  },
+};
 
 const interactiveMenu = {
   items: [],
@@ -52,7 +72,9 @@ const interactiveMenu = {
   requested: false,
   sectionButtonHandler: function (event) {
     if (interactiveMenu.requested) {
-      DOM.render(interactiveMenu.items[event.target.getAttribute("j-index")]);
+      let index = event.target.getAttribute("j-index");
+      DOM.render(interactiveMenu.items[index]);
+      DOM.toggleSection(index);
     }
   },
   descriptionButtonHandler: function (event) {
@@ -70,4 +92,6 @@ const interactiveMenu = {
 };
 
 interactiveMenu.request();
-DOM.sectionButtons.forEach((button) => button.addEventListener("click", interactiveMenu.sectionButtonHandler));
+DOM.sectionButtons.forEach((button) =>
+  button.addEventListener("click", interactiveMenu.sectionButtonHandler)
+);
